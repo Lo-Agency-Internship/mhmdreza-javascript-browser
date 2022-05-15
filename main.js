e42c976ebba160fbdef6d65eedcb7b1561e48ccb
Mohamadreza Naderi <mhmdreza.naderi2002@gmail.com>
2022-05-15 09:29:01 +0430

    fix bug

---
+++ main.js
@@ -0,0 +1,6 @@
+function onclicks( )
+{
+    let color = Math.floor(Math.random()*16666666).toString(16);
+    document.getElementById("backgroundcolor").style.backgroundColor ='#'+color
+    console.log(color)
+}
