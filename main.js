aebc99696e1418d26bc7736a68547792e6dd74f6
Mohamadreza Naderi <mhmdreza.naderi2002@gmail.com>
2022-05-16 14:49:31 +0430

    add some files and images

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-function onclicks( )
-{
-    let color = Math.floor(Math.random()*16666666).toString(16);
-    document.getElementById("backgroundcolor").style.backgroundColor ='#'+color
-    console.log(color)
-}
+// <!-- project one --> hex
+// function onclicks( )
+// {
+//     let color = Math.floor(Math.random()*16666666).toString(16);
+//     document.getElementById("backgroundcolor").style.backgroundColor ='#'+color
+//     console.log(color)
+// }
+// <!-- project two -->
+
+
+
+
+
+
+// start project 2 --> filter 
+
+
+
+
+
+// end of project 2
