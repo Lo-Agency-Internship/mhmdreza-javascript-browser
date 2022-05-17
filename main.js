5a626ff44405510c90686a7381e73589afccc830
Mohamadreza Naderi <mhmdreza.naderi2002@gmail.com>
2022-05-17 11:13:46 +0430

    update files

--- main.js
+++ main.js
@@ -11,11 +11,69 @@
 
 
 
-
 // start project 2 --> filter 
 
 
 
-
+function onclicks1(name) {  
+  if (name!=="all") {
+    switch (name) {
+      
+      case "sweet":
+       document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+       document.querySelectorAll(".sweet").forEach(a=>a.style.display = "initial");
+        break;
+        case "cake":
+       document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+       document.querySelectorAll(".cake").forEach(a=>a.style.display = "initial");
+        break;
+        case "dounat":
+       document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+       document.querySelectorAll(".dounat").forEach(a=>a.style.display = "initial");
+        break;
+        case "cupcake":
+       document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+       document.querySelectorAll(".cupcake").forEach(a=>a.style.display = "initial");
+        break;
+    
+      default:
+        break;
+    }
+  }
+  else 
+  {
+    document.querySelectorAll(".all").forEach(a=>a.style.display = "initial");
+  }
+  
+ }
+ 
+function onclicks2() {
+ let val= document.getElementById("search").value
+ switch (val) {
+   case "sweet":
+    document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".sweet").forEach(a=>a.style.display = "initial");
+     break;
+     case "cake":
+    document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".cake").forEach(a=>a.style.display = "initial");
+     break;
+     case "dounat":
+    document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".dounat").forEach(a=>a.style.display = "initial");
+     break;
+     case "cupcake":
+    document.querySelectorAll(".all").forEach(a=>a.style.display = "none");
+    document.querySelectorAll(".cupcake").forEach(a=>a.style.display = "initial");
+     break;
+     case "all":
+  
+      document.querySelectorAll(".all").forEach(a=>a.style.display = "initial");
+       break;
+ 
+   default:
+     break;
+ }
+}
 
 // end of project 2
